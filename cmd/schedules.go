f9c5d494839b836e1b4f9057dd1e6bd664e143a4
zachwinslett <zwwinslett@gmail.com>
2026-06-29 00:29:07 -0500

    updating my weekly schedule

--- cmd/schedules.go
+++ cmd/schedules.go
@@ -4,6 +4,6 @@ type Schedule = string
 
 const (
 	DailySchedule   Schedule = "0 14 * * *"
-	WeeklySchedule  Schedule = "0 14 * * 0"
+	WeeklySchedule  Schedule = "0 11 * * 1"
 	MonthlySchedule Schedule = "0 14 1 * *"
 )
